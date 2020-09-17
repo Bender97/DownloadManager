cb0659093bbfc7ccd491d82ef81a9ccf16f1885d
fusy <bopallino@gmail.com>
2020-09-17 21:00:01 +0200

    working on ssologin

--- ssologin.py
+++ ssologin.py
@@ -8,7 +8,70 @@ from selenium.webdriver.common.keys import Keys
 import time
 import os
 
-def SSOLogin(driver):
+
+#1 DRIVE TO REQUESTED PAGE
+#2 CHECK IF LOGIN IS NECESSARY
+
+def isPolicyPage(driver):
+    return "/user/policy.php" in driver.current_url
+
+def isLoginRequested(driver):
+    return "/login/index.php" in driver.current_url
+
+def extractHref(text):
+    sub = "href"
+    start = -1
+    
+    for i in range(len(text)):
+        if (text.startswith(sub, i)):
+            start = i+6
+            break
+    if (start<0):
+        print("error: couldn't find login page start href")
+        exit(0)
+
+    sub = "\""
+
+    for i in range(start, len(text)):
+        if (text.startswith(sub, i)):
+            end = i
+            break
+    if (end<0):
+        print("error: couldn't complete login href link")
+        exit(0)
+    link = text[start:end]
+    return link
+
+def clickLinkInsideInnerHTML(elem, driver, delay):
+    link = extractHref(elem.get_attribute("innerHTML"))
+    driver.get(link)
+    time.sleep(delay)
+
+def clickLogin(driver):
+    elem = driver.find_element_by_class_name("login")
+    clickLinkInsideInnerHTML(elem, driver, 5)
+
+def clickShibbox(driver):
+    elem = driver.find_element_by_id("shibbox")
+    clickLinkInsideInnerHTML(elem, driver, 5)
+
+
+def SSOLogin(driver, courseURL):
+    driver.get(courseURL)
+    time.sleep(5)
+    
+    if isPolicyPage(driver):
+        clickLogin(driver)
+        clickShibbox(driver)
+        
+    else:
+        if (isLoginRequested(driver)):
+            print("eeh si")
+            exit(0)
+        else:
+            print("no")
+            exit(0)
+
     while(True):
         try:
             sso_path = "https://elearning.unipd.it/math/auth/shibboleth/index.php"
