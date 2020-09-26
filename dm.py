f536a8b9d762efc38b486ac120daaf6c829365ef
fusy <bopallino@gmail.com>
2020-09-26 18:11:23 +0200

    working on graphics

--- dm.py
+++ dm.py
@@ -6,6 +6,68 @@ from downloadpdf import downloadpdf
 from parsePage import parsePage, printElements
 from setAndCreateFirefoxProfile import setAndCreateFirefoxProfile
 
+from tkinter import *
+import tkinter as tk
+
+
+checkbuttons = []
+checkVar = []
+
+num = 10
+
+def downloadCallback():
+	for i in range(num):
+		print(checkVar[i].get())
+
+def noneBtnCallback():
+	for btn in checkbuttons:
+		btn.deselect()
+
+def allBtnCallback():
+	for btn in checkbuttons:
+		btn.select()
+
+def onFrameConfigure(canvas):
+    '''Reset the scroll region to encompass the inner frame'''
+    canvas.configure(scrollregion=canvas.bbox("all"))
+
+root = tk.Tk()
+canvas = tk.Canvas(root)
+frame = tk.Frame(canvas)
+vsb = tk.Scrollbar(root, orient="vertical", command=canvas.yview)
+canvas.configure(yscrollcommand=vsb.set)
+vsb.pack(side="right", fill="y")
+canvas.pack(side="left", fill="both", expand=True)
+canvas.create_window((4, 4), window=frame, anchor="nw")
+
+frame.bind("<Configure>", lambda event, canvas=canvas: onFrameConfigure(canvas))
+
+for i in range(num):
+	checkVar.append(IntVar())
+	checkbuttons.append(Checkbutton(frame, text = "prova"+str(i),\
+		variable = checkVar[i], onvalue=1, offvalue=0, height=2, width = 20))
+
+
+B = Button(frame, text="Download", command=downloadCallback)
+B.pack(side = tk.BOTTOM)
+
+B_none = Button(frame, text="Select Nothing", command=noneBtnCallback)
+B_none.pack(side = tk.BOTTOM)
+
+B_all = Button(frame, text="Select All", command=allBtnCallback)
+B_all.pack(side = tk.BOTTOM)
+
+
+for i in range(num):
+	checkbuttons[i].pack()
+
+root.mainloop()
+
+
+
+
+exit()
+
 path_to_mozilladriver = "/home/fusy/geckodriver-v0.27.0-linux32/geckodriver"
 fp = setAndCreateFirefoxProfile()
 
